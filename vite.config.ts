0e87debe5a0f5e90e82dc37711d51339464afe83
quyminh <minhquyqt29@gmail.com>
2024-01-16 11:17:25 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+  define: {
+    "global": {},
+    "process": {}
+  },
 })
